9d4687811b847d6b65ac64554cc51c2574049eb8
Caroline Prioleau <caroline@prioleau.com>
2016-11-10 21:31:08 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+  // set score
+  var score = 0;
+  $("#score").html(score + " " + "Points");
+
+  // click event on increase-5
+  $("#increase-5").on("click", function()  {
+    score = score + 5;
+    $("#score").html(score + " " +"Points");
+    console.log(score, "CURRENT SCORE");
+  });
+
+  // click event on decrease-5
+  $("#decrease-5").on("click", function()  {
+    if ((score-5) > 0) {
+      score = score - 5;
+      $("#score").html(score + " " + "Points");
+      console.log(score, "CURRENT SCORE");
+    }
+  });
+
+  // set custom score
+  $("#custom-score-form").on("submit", function(event) {
+    event.preventDefault();
+    var customScore = parseInt($("#custom-score").val());
+    // check if customScore is negative
+    if (customScore >= 0) {
+      score = customScore;
+      $("#score").html(score + " " + "Points");
+      console.log(score, "CUSTOM SCORE");
+    };
+
+  });
+
+});
